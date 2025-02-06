ca5ac949567ce863c1fdbce60e6aa89c2df7afef
Maggi Ovelar <ovelar.romidesk@gmail.com>
2025-02-06 07:24:30 -0300

    delete comment

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Esto debería coincidir con la ruta correcta
+import router from './router'; 
 import store from './store';
 import "./assets/styles.css";
 
